b7cfd102406f64db589e6a86f58c902a9d4fab5f
Aric Johnson <aric87@comcast.net>
2016-10-26 17:05:04 -0400

    edit endpoint protection, changed undefined err to e

--- app/routes/tunes.js
+++ app/routes/tunes.js
@@ -2,7 +2,7 @@ var User = require('../models/user'),
     Song = require('../models/song'),
     isLoggedIn = require('../services'),
     path = require('path'),
-    tunesUploadDir = process.env.OPENSHIFT_DATA_DIR ? '/tunes' : path.resolve(__dirname, '../../views/tunes');
+    tunesUploadDir = process.env.OPENSHIFT_DATA_DIR ? path.join(process.env.OPENSHIFT_DATA_DIR, '/tunes/') : path.resolve(__dirname, '../../views/tunes/');
 
 module.exports = function (app, multipartyMiddleware, fs) {
     app.get('/tunes', isLoggedIn, function (req, res) {
@@ -40,7 +40,7 @@ module.exports = function (app, multipartyMiddleware, fs) {
                   if (err) {
                     reject(err);
                   }else{
-                    newSong[file.fieldName] = createDir;
+                    newSong[file.fieldName] = '/tunes/'+file.name;
                     resolve();
                   }
                 });

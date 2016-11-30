299ad300214ef9f637ebf1d65ae22de54991e04f
Aric Johnson <aric87@comcast.net>
2016-11-29 22:21:02 -0500

    update seed to rework existing DB data

--- config/seed.js
+++ config/seed.js
@@ -1,18 +1,41 @@
 var Band = require('../app/models/band');
 var User = require('../app/models/user');
+var Song = require('../app/models/song');
+var Document = require('../app/models/document');
 var Announcement = require('../app/models/announcement');
 
 exports.run = function(callback, errback) {
-  Band.create({
-    name:'Highland Light',
-    bandCode:"hlpb",
-    active:true
-  }, function(err){
-    if (err){
-      console.log(err)
-    }
-    callback()
-  })
+  var band = new Promise((resolve, reject)=>{
+    Band.findOne({bandCode:'hlpb'}, function(err,band){
+      if(err){
+        reject(err);
+      }
+      resolve(band);
+    });
+  });
+  band.then((band) =>{
+    Document.find({}, function(err, documents){
+        if(err){
+          reject(err);
+        }
+        var documentsIds = documents.map(function(document){
+          document.band = band._id;
+          document.save(function(err){
+            if(err){
+              console.log(err);
+            }
+          });
+          return document._id;
+        });
+        band.documents = documentsIds;
+        band.save(function(err){
+          if(err){
+            return err;
+          }
+          callback();
+      });
+      });
+    });
 };
 
 if (require.main === module) {
